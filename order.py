a11595f4112fa57947442d99a0114261cb199cc1
mark <mark.kimani3@student.moringaschool.com>
2025-05-20 22:55:21 +0300

    Update order.py

--- order.py
+++ order.py
@@ -1,17 +1,34 @@
 class Order:
+
+    all=[]
+
     def __init__(self, customer, coffee, price):
-        self.customer = customer
-        self.coffee = coffee
-        self.price = price
+        from customer import Customer
+        from coffee import Coffee
 
         if not isinstance(customer, customer):
             raise TypeError("customer must be a Customer instance")
+
         if not isinstance(coffee, coffee):
             raise TypeError("coffee must be a Coffee instance")
+
+
         if not isinstance(price, float):
             raise TypeError("price must be a float")
+
+
         if not (1.0 <= price <= 10.0):
             raise ValueError("price must be between 1.0 and 10.0")
+
+
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price 
+        Order.all.append(self)
+
+
+
+
 @property
 def customer(self):
     return self._customer
