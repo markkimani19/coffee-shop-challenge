f73e185a46895bf0a4534e364e3666e2df73422f
mark <mark.kimani3@student.moringaschool.com>
2025-05-20 23:16:18 +0300

    Changed debug.py

--- customer.py
+++ customer.py
@@ -1,28 +1,23 @@
 class Customer:
     all = []
 
-
     def __init__(self, name):
         self.name = name
         Customer.all.append(self)
-    
-    def __repr__(self):
-        return f"customer name = {self.name}"
 
+    def __repr__(self):
+        return f"Customer(name='{self.name}')"
 
     @property
     def name(self):
         return self._name
 
     @name.setter
-
-    def customerName(name):
-
-        if isinstance(name,str) and 1 <= len(name) <= 15:
-            return name
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
         else:
-            raise ValueError("Character Must be a string between 1 to 25")
-
+            raise ValueError("Name must be a string between 1 and 15 characters.")
 
     def orders(self):
         return [order for order in Order.all if order.customer == self]
@@ -32,6 +27,7 @@ class Customer:
 
     def create_order(self, coffee, price):
         return Order(self, coffee, price)
+
         
 # customer = Customer("Mark")
 # print(customer)
--- debug.py
+++ debug.py
@@ -2,16 +2,5 @@ from customer import Customer
 from coffee import Coffee
 from order import Order
 
-mark = Customer("Mark")
-titus = Customer("Titus")
-
-edward = Coffee("Edward")
-asenath = Coffee("Asenath")
-
-mark.create_order(edward, 3.5)
-mark.create_order(asenath, 4.0)
-titus.create_order(edward, 5.5)
-
-print("Mark's coffees:", mark.coffees())
-print("Latte orders:", edward.orders())
-print("Latte average price:", edward.average_price())
+marky = Customer("Mark")
+print(marky.name)
